3509344dc6cd4528df4b078b0e1677d4b3eeb885
sera2002 <ouioui2@g.skku.edu>
2024-06-24 11:22:41 +0900

    dropout을 hidden_dropout_prob으로 변경

--- trainer_crf.py
+++ trainer_crf.py
@@ -33,8 +33,7 @@ class TrainerCRF(object):
                                                         finetuning_task=args.task,
                                                         id2label={str(i): label for i, label in enumerate(self.label_lst)},
                                                         label2id={label: i for i, label in enumerate(self.label_lst)},
-                                                        dropout=0.1,
-                                                        hidden_size=768)
+                                                        hidden_dropout_prob=0.1)
         self.model = KobertCRF(config=self.config, num_classes=self.num_labels, vocab=vocab)
 
         # GPU or CPU
